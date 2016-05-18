2541846b3c190730afd430a87f226a462fc3b1b8
Umar <uzbhutta@uwaterloo.ca>
2016-05-18 12:22:15 -0400

    Implemented MicSensor.h and MicSensor.cpp - MicSensor library

---
+++ package/MIC/MicSensor.cpp
@@ -0,0 +1,91 @@
+/**
+* MicSensor.cpp - Xlight MIC sensor lib
+*
+* Created by Umar Bhutta <umar.bh@datatellit.com>
+* Copyright (C) 2015-2016 DTIT
+* Full contributor list:
+*
+* Documentation:
+* Support Forum:
+*
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* version 2 as published by the Free Software Foundation.
+*
+*******************************
+*
+* REVISION HISTORY
+* Version 1.0 - Created by Umar Bhutta <umar.bh@datatellit.com>
+*
+* DESCRIPTION
+* 1. support PIR MIC sensor (3 pin)
+* 2. MicSensor::read() functionality has been emulated from Adafruit's Mic Amplifier
+*    example. Audio signal from the amp is a varying voltage. Multiple measurements
+*    are taken to find the min and max (peak-to-peak) amplitude of the signal.
+*		 The difference between min and max samples is computed and converted to volts.
+*	3. A 50ms window is used (20Hz frequency - lower limit of human hearing).
+*
+**/
+
+#include "MicSensor.h"
+
+MicSensor::MicSensor(uint8_t pin, uint8_t type)
+{
+	_pin = pin;
+	_type = type;
+	firstreading = true;
+}
+
+void MicSensor::begin()
+{
+	pinMode(_pin, INPUT);
+	_lastreadtime = 0;
+	_lastValue = 0;
+	startMillis = millis();
+}
+
+float MicSensor::getLoudnessVoltage()
+{
+	float loudnessVoltage = read();
+	return loudnessVoltage;
+}
+
+float MicSensor::read()
+{
+	unsigned long currenttime;
+	currenttime = millis();
+
+	if (currenttime < _lastreadtime)
+	{
+		_lastreadtime = 0;
+	}
+	if (firstreading || ((currenttime - _lastreadtime) >= 25)) 
+		//Mic readings will be collected in IntervalTimer function from an interrupt context, which is very fast - currently 25s.
+	{
+		// collect data for 50 mS
+		while (currenttime - startMillis < SAMPLE_WINDOW)
+		{
+			sample = analogRead(_pin);
+			if (sample < 1024)  // toss out bad readings
+			{
+				if (sample > signalMax)
+				{
+					signalMax = sample;  // save just the max levels
+				}
+				else if (sample < signalMin)
+				{
+					signalMin = sample;  // save just the min levels
+				}
+			}
+		}
+
+		firstreading = false;
+		_lastreadtime = millis();
+
+	}
+
+	peakToPeak = signalMax - signalMin;  // max - min = peak-peak amplitude
+	float volts = (peakToPeak * 3.3) / 1024;  // convert to volts
+
+	return volts;
+}
---
+++ package/MIC/MicSensor.h
@@ -0,0 +1,33 @@
+//  MicSensor.h - Xlight MIC sensor lib
+
+#ifndef MicSensor_h
+#define MicSensor_h
+
+#include "application.h"
+
+class MicSensor {
+private:
+	uint8_t _pin;
+	uint8_t _type;
+	bool firstreading;
+	unsigned long _lastreadtime;
+	uint16_t _lastValue;
+
+	uint8_t peakToPeak = 0;   // peak-to-peak level
+	uint8_t signalMax = 0;
+	uint8_t signalMin = 1024;
+	const uint8_t SAMPLE_WINDOW = 50; // Sample window width in mS (50 mS = 20Hz)
+	uint8_t sample;
+	unsigned long startMillis;  // Start of sample window
+
+	bool pirState = 0;
+
+	uint16_t read();
+
+public:
+	MicSensor(uint8_t pin, uint8_t type = 0);
+	void begin();
+	uint16_t getLoudnessVoltage();
+};
+
+#endif /* MicSensor_h */
